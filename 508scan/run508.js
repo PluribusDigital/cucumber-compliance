bcb2b837551b66f463d360e491c0d361d7f5c15b
kmelendez123 <kmelendez@stsiinc.com>
2017-09-27 13:33:36 -0400

    add 508scan

---
+++ 508scan/run508.js
@@ -0,0 +1,27 @@
+var pa11y = require('pa11y');
+
+// Create a test instance with some default options
+var test = pa11y({
+
+	// Log what's happening to the console
+	log: {
+		debug: console.log.bind(console),
+		error: console.error.bind(console),
+		info: console.log.bind(console)
+	},
+
+	// Run some actions before the tests
+	actions: [
+		'click element #test>table>tbody>tr>td>p>a',
+		'wait for url to be https://teas.uspto.gov/forms/teasplus'
+	]
+
+});
+
+// Test https://teas.uspto.gov/
+test.run('https://teas.uspto.gov', function(error, result) {
+	if (error) {
+		return console.error(error.message);
+	}
+	console.log(result);
+});
