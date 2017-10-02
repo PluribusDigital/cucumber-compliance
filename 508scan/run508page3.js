c3b71f1b833a2ac4cfff8948308cfdc92d23fdc3
kmelendez123 <kmelendez@stsiinc.com>
2017-10-02 13:55:45 -0400

    add 508scan page and update browser version

--- 508scan/run508page3.js
+++ 508scan/run508page3.js
@@ -1,8 +1,6 @@
 var pa11y = require('pa11y');
-
 // Create a test instance with some default options
 var test = pa11y({
-
 	// Log what's happening to the console
 	log: {
 		debug: console.log.bind(console),
@@ -13,15 +11,15 @@ var test = pa11y({
 	// Run some actions before the tests
 
 	actions: [
-		'click element #attNeededYes',
-    'click element .button',
-    'wait for url to not be https://teas.uspto.gov/forms/teasplus'
+    'set field input[name="form.owner[0].internalAddr"] to ABC 123',
+    'click element .button[value="Continue"]',
+    'wait for url to be https://teas.uspto.gov/forms/teas.service'
 	]
 
 });
 
 // Test https://teas.uspto.gov/forms/teasplus
-test.run('https://teas.uspto.gov/forms/teasplus', function(error, result) {
+test.run('https://teas.uspto.gov/forms/teas.service?', function(error, result) {
 	if (error) {
 		return console.error(error.message);
 	}
